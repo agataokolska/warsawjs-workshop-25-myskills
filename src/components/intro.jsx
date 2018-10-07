aec7326ff4f2bdb56298eb6c1d8a5f6cbbe314d2
agata okolska <agata.okolska@gmail.com>
2018-10-07 13:54:38 +0200

    create basic components structure

---
+++ src/components/intro.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class IntroComponent extends Component {
+
+    render() {
+
+        return (
+            <div>
+                <h2>Hello World</h2>
+            </div>
+        )
+    }
+}
--- src/main.js
+++
@@ -1,3 +0,0 @@
-import { TEST } from './intro'
-
-console.log(TEST)
